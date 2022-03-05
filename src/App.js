c60a408b8430baa1fa9efc22f4e424647bae1e08
Kamil <kamilbednarz91@gmail.com>
2022-03-05 09:43:01 -0600

    sign in bug fix

--- src/App.js
+++ src/App.js
@@ -96,11 +96,7 @@ const app = new Clarifai.App({
  apiKey: "",
 });
 
- 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
+const initialState = {
       input: '',
       imageURL:'',
       box: {},
@@ -112,8 +108,13 @@ class App extends Component {
         email: '',
         entries: 0,
         joined: ''
-      }
-    }
+  }
+}
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = initialState;
   }
 
   loadUser = (data) => {
@@ -165,6 +166,7 @@ class App extends Component {
           .then(count => {
             this.setState(Object.assign(this.state.user, { entries: count}))
           })
+          .catch(console.log)
       }
      this.displayFaceBox(this.calculateFaceLocation(response))
      })
@@ -173,7 +175,7 @@ class App extends Component {
 
   onRouteChange = (route) => {
     if (route === 'signout') {
-      this.setState({isSignedIn: false})
+      this.setState(initialState)
     } else if (route === 'home') {
       this.setState({isSignedIn: true})
     }
